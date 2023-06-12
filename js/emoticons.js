0836c3a7c8064b1fb9849939001784e653320014
Natalina686 <108237928+Natalina686@users.noreply.github.com>
2023-06-13 00:06:51 +0200

    added html, js, emoticons

---
+++ js/emoticons.js
@@ -0,0 +1,9 @@
+let emoticonArray = [ '🐸 ', '🐮 ', '🐨 ', '🐯 ', '🐼 ', '🐵 ', '🐷 ',];
+
+function getRandomEmoticon(min, max) {
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  let number = Math.round(rand);
+
+  return emoticonArray[number];
+};
+export { getRandomEmoticon };
---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict'
+
+import { getRandomEmoticon } from './js/emoticons.js';
+
+const inputData = document.querySelector('.input');
+const form = document.querySelector('form');
+const arrayBlock = document.querySelector('.array');
+const setBlock = document.querySelector('.set');
+
+let array = [];
+let set = new Set();
+
+inputData.value = getRandomEmoticon(0, 5);
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  array.push(inputData.value);
+  set.add(inputData.value);
+
+  arrayBlock.textContent = `Типовий Array: [ ${array} ]`;
+  let setContent = [...set];
+
+  setBlock.textContent = `Типовий Set: [ ${setContent} ]`;
+
+  inputData.value = getRandomEmoticon(0, 5);
+
+})
